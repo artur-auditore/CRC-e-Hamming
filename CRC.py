00505716fd70ba410096ad8e7aa0828e887b42bb
artur-auditore <defteros7@gmail.com>
2019-12-10 12:57:47 -0300

    other changes

--- CRC.py
+++ CRC.py
@@ -2,31 +2,32 @@
 def xor(a, b):
     result = []
 
+    #percorre todos os bits, se cada posição for igual, é zero, se não é 1
     for i in range(1, len(b)):
         if a[i] == b[i]:
             result.append('0')
         else:
             result.append('1')
     
-    return ''.join(result)
+    return ''.join(result) #retorna como uma string
 
 #calculo do grau 
 def calcula_grau(gx):
     ct = 0
     grau = 0
     for i in range(len(gx)):
-        if gx[i] == '0' and ct == '0':
+        if gx[i] == '0' and ct == 0:
             pass
         else:
             ct += 1
             grau += 1
     return grau - 1
 
-#verificação do gx
+#validação do gx
 def verifica(gx):
     if (len(gx) >= 2 and gx[0] == '1' and gx[len(gx)-1] == '1'):
         return True
-    else:
+    else:   
         return False
 
 #forma a palavra para o que envia
@@ -40,16 +41,17 @@ def palavra_rec(msg, crc):
 
 #faz a divisão
 def divisao(dividendo, divisor):
-    pick = len(divisor) 
-    temp = dividendo[0 : pick] 
-   
+    pick = len(divisor) #pega o tamanho do divisor
+    temp = dividendo[0 : pick] #pega o primeiro 'pedaço' do dividendo do mesmo tamanho do divisor
+
+    #enquanto 'pick' for menor que o tamanho do dividendo mais um bit vai sendo adicionado a 'temp'
+    #que é o resto da divisão
     while pick < len(dividendo): 
-   
         if temp[0] == '1':  
             temp = xor(divisor, temp) + dividendo[pick] 
         else: 
             temp = xor('0'*pick, temp) + dividendo[pick] 
-        pick += 1
+        pick += 1 #incrementa pra pegar a próxima posição do dividendo
 
     if temp[0] == '1': 
         temp = xor(divisor, temp) 
